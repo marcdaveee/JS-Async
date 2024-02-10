04fc55006f488ec5fd008eac2b8bb3a645b46d8c
Marc Dave <davevalencia17@gmail.com>
2024-02-10 15:02:51 +0800

    include fetch api sample

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-let app = document.querySelector("#app");
+// Get a promise
+
+const getPromise = () => {
+  // fetch something
+
+  return new Promise((resolve, reject) => {
+    resolve("Resolved data!");
+    // reject("Some errors");
+  });
+};
+
+// getPromise().then(
+//   (data) => {
+//     console.log(data);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+// Cleaner format
+getPromise()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//   using fetch API
+
+function getTodo() {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Resolved: ", data);
+    })
+    .catch((err) => {
+      console.log("Error", err);
+    });
+}
+
+getTodo();
